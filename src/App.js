7d304b0a95f57ba0546bdde17783a5f5b6e540e8
Sruba Mohapatra <sruba209239@gmail.com>
2020-03-26 01:02:34 +0530

    basic setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from "./Person/Person";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi. This is a react App</h1>
+        <p>Is it working ?</p>
+        <Person />
+      </div>
+    );
+    // return React.createElement('div', null, 'h1', 'Hi this is a react code');
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi this is a react code'));
+  }
+}
+
+export default App;
