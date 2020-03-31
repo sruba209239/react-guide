2dae51eb28fcfb9b28bf8c8b42fd10a5e5cf718f
Sruba Mohapatra <sruba209239@gmail.com>
2020-03-31 23:38:52 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi. This is a react App</h1>
         <p>Is it working ?</p>
-        <Person />
+        <Person name="Sruba" age="28"/>
+        <Person name="Pinki" age="25">My hobbies are racing</Person>
+        <Person name="Bubu" age="24"/>
       </div>
     );
     // return React.createElement('div', null, 'h1', 'Hi this is a react code');
