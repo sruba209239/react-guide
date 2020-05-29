def061a223836c5067f75a06daff32872731db46
Sruba Mohapatra <sruba209239@gmail.com>
2020-05-30 00:35:24 +0530

    60

--- src/App.js
+++ src/App.js
@@ -1,69 +1,89 @@
-import React, { Component } from 'react';
-import './App.css';
-import './Person/Person.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./Person/Person.css";
 import Person from "./Person/Person";
 
 class App extends Component {
   state = {
     persons: [
-      { name: 'Sruba', age: 30 },
-      { name: 'Pinki', age: 26 },
-      { name: 'Bubu', age: 24 }
+      { id: "saa", name: "Sruba", age: 30 },
+      { id: "dsd", name: "Pinki", age: 26 },
+      { id: "fdf", name: "Bubu", age: 24 },
     ],
-    other: 'other state',
-    showPersons: false
-  }
+    other: "other state",
+    showPersons: false,
+  };
 
-  switchNameHandler = (newName) => {
-    let newPersons = this.state.persons.slice();
-    newPersons[0].name = newName;
-    newPersons[2].age = 27;
-    this.setState({
-      persons: newPersons
-    });
-  }
+  deletePersonHandler = (index) => {
+    const persons = this.state.persons.slice(0);
+    persons.splice(index, 1);
+    this.setState({ persons: persons });
+  };
 
-  setNameHandler = (event) => {
+  setNameHandler = (event, id) => {
+    const personIndex = this.state.person.findIndex((item) => item.id === id);
+    const person = {
+      ...this.state.person[personIndex],
+    };
+    // const person = Object.assign({}, this.state.person[personIndex]);
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Sruba', age: 30 },
-        { name: event.target.value, age: 26 },
-        { name: 'Bubu', age: 24 }
-      ]
+      persons: persons,
     });
-  }
+  };
 
   togglePersonsHandler = () => {
     this.setState({
-      showPersons: !this.state.showPersons
+      showPersons: !this.state.showPersons,
     });
-  }
+  };
 
   render() {
     const myStyle = {
-      backgroundColor: 'lightgreen',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+      backgroundColor: "lightgreen",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
     };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                clickMethod={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                nameMethod={(event) => this.setNameHandler(event, person.id)}
+              />
+            );
+          })}
+          {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} clickMethod={this.switchNameHandler.bind(this, 'name2')} nameMethod={this.setNameHandler}>My hobbies are racing</Person>
+
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} /> */}
+        </div>
+      );
+    }
 
     return (
       <div className="App">
         <h1>Hi. This is a react App</h1>
         <p>Is it working ?</p>
-        <button style={myStyle} onClick={this.togglePersonsHandler}>Toggle Persons</button> {/* this may be inefficient*/}
-
-        {this.state.showPersons ?
-          <div>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} clickMethod={this.switchNameHandler.bind(this, 'name2')} nameMethod={this.setNameHandler}>My hobbies are racing</Person>
-
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-          </div> : null
-        }
-
+        <button style={myStyle} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>{" "}
+        {/* this may be inefficient*/}
+        {persons}
         <p>{this.state.other}</p>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 const person = (props) => {
-    return (
-        <div className="Person">
-            <p onClick={props.clickMethod}>I am {props.name} and i am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <p>
-                <input onChange={props.nameMethod} value={props.name}></input>
-            </p>
-        </div>
-    )
+  return (
+    <div className="Person">
+      <p onClick={props.clickMethod}>
+        I am {props.name} and i am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <p>
+        <input onChange={props.nameMethod} value={props.name}></input>
+      </p>
+    </div>
+  );
 };
 
 export default person;
