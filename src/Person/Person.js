86b3cbf299fe0ba2d35cf1efe8a2ca460d716b42
Sruba Mohapatra <sruba209239@gmail.com>
2020-07-03 21:39:39 +0530

    72

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,21 +1,36 @@
 import React from "react";
+import styled from 'styled-components';
 
-let myStyle = {
-  '@media(min-width:500px)': {
-    width: '450px'
+const StyledDiv = styled.div`
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 15px;
+  text-align: center;
+  @media (min-width: 500px) {
+    width: 450px
   }
-};
+`;
+
+// let myStyle = {
+//   '@media(min-width:500px)': {
+//     width: '450px'
+//   }
+// };
 const person = (props) => {
   return (
-    <div className="Person" style={myStyle}>
-      <p onClick={props.clickMethod}>
-        I am {props.name} and i am {props.age} years old!
+    <StyledDiv>
+      <div className="Person">
+        <p onClick={props.clickMethod}>
+          I am {props.name} and i am {props.age} years old!
       </p>
-      <p>{props.children}</p>
-      <p>
-        <input onChange={props.nameMethod} value={props.name}></input>
-      </p>
-    </div>
+        <p>{props.children}</p>
+        <p>
+          <input onChange={props.nameMethod} value={props.name}></input>
+        </p>
+      </div>
+    </StyledDiv>
   );
 };
 
