aafe96599c58589e4a3a5b1f54b02d05fac90957
Sruba Mohapatra <sruba209239@gmail.com>
2020-07-28 23:38:09 +0530

    93

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,6 +3,27 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
+
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('[Persons.js] getDerivedStateFromProps');
+  //   return state;
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return { message: "snapshot" };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
   render() {
     console.log('[Persons.js] rendering...');
     return this.props.persons.map((person, index) => {
