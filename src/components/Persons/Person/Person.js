3580d6286a4a1cdbd9f9765666bad6bdc2adb6f8
Sruba <sruba@pop-os.localdomain>
2020-08-10 19:42:24 +0530

    104

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
-import classes from './Person.css';
+import React, { Component, Fragment } from "react";
+import classes from "./Person.css";
+// import { Aux } from "../../../hoc/Auxiliary";
 
 class Person extends Component {
   render() {
-    console.log('[Person.js] rendering...');
+    console.log("[Person.js] rendering...");
     return (
-      <div className={classes.Person}>
-        <p onClick={this.props.click}>
+      // <div className={classes.Person}>
+      <Fragment>
+        <p key="item1" onClick={this.props.click}>
           I'm {this.props.name} and I am {this.props.age} years old!
-      </p>
-        <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+        </p>
+        <p key="item2">{this.props.children}</p>
+        <input
+          key="item3"
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Fragment>
+      // </div>
     );
   }
-};
+}
 
 export default Person;
