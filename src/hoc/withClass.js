b1296f92a2cfedb1c309eb13824a6329adefffc6
Sruba <sruba209239@gmail.com>
2020-08-10 22:32:09 +0530

    test

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -3,7 +3,7 @@ import React from "react";
 // const withClass = (props) => (
 //   <div className={props.classes}>{props.children}</div>
 // );
-
+// test
 const withClass = (WrappedComponent, className) => {
   return (props) => (
     <div className={className}>
